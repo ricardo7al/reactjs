81ddaee263e298dac6d20d90d03c0706634dcd48
unknown <ricardo7almail.com>
2020-04-02 23:12:09 -0300

    Updates

---
+++ precache-manifest.a4bf0c13a59abaa41dc7314b70305285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7e819a4961dc8c3f006a181db74702",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "fe37a9c2df57f7f4525d",
+    "url": "/reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14b7b17c50080b063e9f",
+    "url": "/reactjs/static/js/2.035dffc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs/static/js/2.035dffc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe37a9c2df57f7f4525d",
+    "url": "/reactjs/static/js/main.ebff926b.chunk.js"
+  },
+  {
+    "revision": "b22e9d07d27f2581260b",
+    "url": "/reactjs/static/js/runtime-main.7f266d2c.js"
+  }
+]);
